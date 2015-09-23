3b98ea0a8fc78f6cf7230085f9f19645e682d3e6
njaggars <njaggars@gmail.com>
2015-09-22 23:27:48 -0700

    docs and refactor

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-module.exports = function openChromeTab(url) {
-  var exec = require('child_process').exec;
-  var scriptPath = require('path').resolve(__dirname, 'open-chrome-tab.jxa');
-  exec('osascript -l JavaScript ' + scriptPath + ' ' + url, function(err, stdout, stderr) {
+var proc = require('child_process');
+var path = require('path');
+
+/**
+ * Opens a tab in google chrome pointing at `url`. If this script has been used
+ * recently, and the previously opened tab is still available, it will be reused.
+ * @param {string} url
+ * @param {function} callback executed when the open script terminates
+ */
+module.exports = function openChromeTab(url, callback) {
+  var scriptPath = path.resolve(__dirname, 'open-chrome-tab.jxa');
+  proc.exec(['osascript', '-l', 'JavaScript', scriptPath, url].join(' '), function(err, stdout, stderr) {
     if(err) throw err;
+    callback();
   });
 }
