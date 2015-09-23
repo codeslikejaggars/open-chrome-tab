e60ae7282734c6fa76ef4317dc14a8121e93e8d4
njaggars <njaggars@gmail.com>
2015-09-22 18:23:13 -0700

    fixed typo

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 module.exports = function openChromeTab(url) {
   var exec = require('child_process').exec;
   var scriptPath = require('path').resolve(__dirname, 'open-chrome-tab.jxa');
-  exec('osascript -l Javascript ' + scriptPath + ' ' + url, function(err, stdout, stderr) {
+  exec('osascript -l JavaScript ' + scriptPath + ' ' + url, function(err, stdout, stderr) {
     if(err) throw err;
   });
 }
